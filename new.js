0dce5a696d0583393ccb0e8fdfe10a899827b076
Copter <copterpanyphon@gmail.com>
2024-06-23 11:21:49 +0700

    Java script
    make java script more make sense and change decimal to 2 unit

--- new.js
+++ new.js
@@ -9,7 +9,9 @@ button.onclick = () =>{
     height = Number(height.value);
     let num = 2;
     let number = Math.pow(height, num)
-    let BMI = weight / number;
+    let BMI = weight / number * 10000;
+    BMI = BMI.toFixed(2);
+    
     
     return text.textContent = BMI;
     
